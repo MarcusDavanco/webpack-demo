44797dd007cb15583cab8bc6ae017091ea335695
Marcus Davanço <4138199+MarcusDavanco@users.noreply.github.com>
2021-06-20 06:27:23 -0300

    feat: output management

--- src/index.js
+++ src/index.js
@@ -1,35 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import awesomeFace from './awesome-face.png';
-import csvData from './sample.csv';
-import xmlSample from './sample.xml';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output TOML Example
-console.log(toml.owner.name); // output Tom Prest-Werner
-
-console.log(yaml.title); // output YAML Example
-console.log(yaml.owner.name); // output Tom Prest-Werner
-
-console.log(json.title); // output JSON5 Example
-console.log(json.owner.name); // output Tom Preston-Werner
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Loadash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], '');
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
-  const myImg = new Image();
-  myImg.src = awesomeFace;
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-  console.log(csvData);
-  console.log(xmlSample);
-
-  element.appendChild(myImg);
+  element.appendChild(btn);
 
   return element;
 }
