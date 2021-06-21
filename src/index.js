68b2ad60d69c0eef6d8b561399934003ec7d9ea3
Marcus Davanço <4138199+MarcusDavanco@users.noreply.github.com>
2021-06-20 23:32:33 -0300

    feat: code-splitting dynamic imports

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-import _ from 'lodash';
-import printMe from './print';
-
-function component() {
+function getComponent() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
+  return import('lodash')
+    .then(({ default: _ }) => {
+      const element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+      element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
+      return element;
+    })
+    .catch((error) => 'An error occurred while loading the component');
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+});
